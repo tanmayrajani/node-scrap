85e9a1e449288f75081518a38102c192abd0ca3b
Tanmay Rajani <rajani.tanmay@gmail.com>
2015-04-19 23:44:40 +0530

    updated with little changes; works perfect :100:

--- cricbuzz-scrap-automated.js
+++ cricbuzz-scrap-automated.js
@@ -1,5 +1,5 @@
 // Logs IPL match score from cricbuzz into a text file as well as on console.
-
+//var x = 14600;
 var request = require("request"),
 	cheerio = require("cheerio"),
 	fs = require('fs'),
@@ -62,7 +62,7 @@ var timetable = [
 for (var i = 0; i < timetable.length; i++) {
 	if(timetable[i][0]==new Date().getDate() && timetable[i][1]==new Date().getMonth()+1){
 		if(new Date().getHours()>=20 && new Date().getHours()<=23){
-			x = timetable[i][3];
+			x = timetable[i+1][3];
 			break;
 		}
 
@@ -75,7 +75,7 @@ for (var i = 0; i < timetable.length; i++) {
 		x = timetable[i-1][3];
 	}
 };
-
+//console.log(x);
 var url = "http://www.cricbuzz.com/live-cricket-scorecard/" + x;
 	
 setInterval(function () {
@@ -83,6 +83,7 @@ setInterval(function () {
 		if (!error) {
 			var $ = cheerio.load(body),
 			score = $("table.col-md-12 thead h4").text();
+			//score = $(".batteamdesc").text();
 			var index = score.indexOf("Match Info");
 			if(index != -1) score = score.substr(0, index); // removing Match Info form score text
 			var index2 = score.indexOf("20 overs)");
