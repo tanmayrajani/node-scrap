daf93c0a6a969eec85b6cf718091f47aadb41248
Tanmay Rajani <rajani.tanmay@gmail.com>
2015-08-08 01:53:36 +0530

    working great :dart:
    100 records inserted to collection

--- fhyo/index.js
+++ fhyo/index.js
@@ -1,12 +1,33 @@
 var request = require("request"),
 	cheerio = require("cheerio");
 var url = "http://fuckinghomepage.com/random";
+var MongoClient = require('mongodb').MongoClient;
 
-request(url, function (error, response, body) {
+function fuckingHomepageDataBitches () {
+	request(url, function (error, response, body) {
 		if (!error) {
 			var $ = cheerio.load(body);
+			var page = response.request.uri.path.split('/');
+			page = page[page.length-1];
 			var imgurl = $(".PostBody p:nth-child(17) a").attr("href");
 			console.log(imgurl);
-			console.log(response.request.uri);
+			console.log(page);
+			MongoClient.connect('mongodb://localhost:27017/fh', function(err, db) {
+	    		if(err) throw err;
+	    		db.collection('iah').find({"date": page}).each(function (err,doc) {
+	    			if (doc != null && page!="random" && page!="undefined") {
+						console.dir("Hit!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+					} else {
+						db.collection('iah').insert({"date":page,"url":imgurl}, function (err, result) {
+							console.log("Inserted!");
+						})
+					}
+	    		})
+	       	});
 		}
 	});
+
+	setTimeout(fuckingHomepageDataBitches, 86);
+}
+
+fuckingHomepageDataBitches();
