9882cd6e695e46a06b6fe84dec375d1ff3ad8689
Tanmay Rajani <rajani.tanmay@gmail.com>
2017-08-13 10:26:34 +0530

    removes node_modules; indent fix for index.js

--- fhyo/index.js
+++ fhyo/index.js
@@ -3,31 +3,36 @@ var request = require("request"),
 var url = "http://fuckinghomepage.com/random";
 var MongoClient = require('mongodb').MongoClient;
 
-function fuckingHomepageDataBitches () {
+function getFHData() {
 	request(url, function (error, response, body) {
 		if (!error) {
 			var $ = cheerio.load(body);
 			var page = response.request.uri.path.split('/');
-			page = page[page.length-1];
+			page = page[page.length - 1];
 			var imgurl = $(".PostBody p:nth-child(17) a").attr("href");
 			console.log(imgurl);
 			console.log(page);
-			MongoClient.connect('mongodb://localhost:27017/fh', function(err, db) {
-	    		if(err) throw err;
-	    		db.collection('iah').find({"date": page}).each(function (err,doc) {
-	    			if (doc != null && page!="random" && page!="undefined") {
+			MongoClient.connect('mongodb://localhost:27017/fh', function (err, db) {
+				if (err) throw err;
+				db.collection('iah').find({
+					"date": page
+				}).each(function (err, doc) {
+					if (doc != null && page != "random" && page != "undefined") {
 						console.dir("Hit!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 					} else {
-						db.collection('iah').insert({"date":page,"url":imgurl}, function (err, result) {
+						db.collection('iah').insert({
+							"date": page,
+							"url": imgurl
+						}, function (err, result) {
 							console.log("Inserting!");
 						})
 					}
-	    		})
-	       	});
+				})
+			});
 		}
 	});
 
-	setTimeout(fuckingHomepageDataBitches, 86);
+	setTimeout(getFHData, 86);
 }
 
-fuckingHomepageDataBitches();
+getFHData();
